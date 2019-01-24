2672b61ad00cc077685e20d75a3a60e454e6abe1
Winson <wdieu@ucsd.edu>
2019-01-24 10:13:05 -0800

    changed functionality of javascript button

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,7 +11,26 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("a.thumbnail").click(projectClick);
+		$("#testjs").text("Ya Yeet.");
+		$("#testjs").addClass("active");
 	});
+
+function projectClick(e)
+{
+e.preventDefault();
+$(this).css("background-color", "#7fff00");
+var containingProject = $(this).closest(".project");
+var description = $(containingProject).find(".project-description");
+
+if(description.length == 0)
+{
+	$(containingProject).append("<div class = project-description'><p>Description of the project.</p></div>");
+}
+else {
+	description.html("<p>Stop clicking on me! You jsut did it at " + (new Date()) + "</p>");
+}
+}
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
